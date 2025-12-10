ea93ed6b77cac62495b8424377ebd0a813b73358
Cardona08 <lparturocardona@gmail.com>
2025-12-10 10:19:09 -0600

    se completo reta correctamente

---
+++ Resta.py
@@ -0,0 +1,11 @@
+print("Operacion Resta")
+
+print("Se debera Ingresar 3 Numeros ya que sera la resta de 3 numeros ")
+
+num1=float(input("Ingresa tu primer numero: "))
+num2=float(input("Ingresa tu segundo numero: "))
+num3=float(input("Ingresa tu tercer numero: "))
+
+resta = num1 - num2 - num3
+
+print("La resta de tus tres Numeros es:",resta)
