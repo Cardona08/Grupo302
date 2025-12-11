035a84024efe2c27ba41530c07d111cd49cba288
Cardona08 <lparturocardona@gmail.com>
2025-12-11 07:24:57 -0600

    Se Completo Division Exitosamente

---
+++ Divicion.py
@@ -0,0 +1,11 @@
+print("Operacion Division")
+
+print("Se debera Ingresar 3 Numeros ya que sera la Division de 3 numeros ")
+
+num1=float(input("Ingresa tu primer numero: "))
+num2=float(input("Ingresa tu segundo numero: "))
+num3=float(input("Ingresa tu tercer numero: "))
+
+division = num1 / num2 / num3
+
+print("La division de tus tres Numeros es:",division)
