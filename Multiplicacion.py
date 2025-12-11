eae716772f2a85477da200d56c28090663760281
Cardona08 <lparturocardona@gmail.com>
2025-12-11 07:21:13 -0600

    Se completo Multiplicacion Correctamente

---
+++ Multiplicacion.py
@@ -0,0 +1,11 @@
+print("Operacion Multiplicacion")
+
+print("Se debera Ingresar 3 Numeros ya que sera la multiplicacion de 3 numeros ")
+
+num1=float(input("Ingresa tu primer numero: "))
+num2=float(input("Ingresa tu segundo numero: "))
+num3=float(input("Ingresa tu tercer numero: "))
+
+suma = num1 * num2 * num3
+
+print("La multiplicacion de tus tres Numeros es:",suma)
